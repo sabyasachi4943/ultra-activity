f69604131c8ef183cf40f5456a2833153e4626d9
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-09-29 22:11:48 +0600

    question component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Activity from './components/Activity/Activity';
+import Question from './components/Question/Question';
 
 
 function App() {
   return (
     <div className="App">
       <Activity></Activity>
+      <Question></Question>
     </div>
   );
 }
---
+++ src/components/Question/Question.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Question.css';
+const Question = () => {
+  return (
+    <div>
+        
+    </div>
+  );
+};
+
+export default Question;
